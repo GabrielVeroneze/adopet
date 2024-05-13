200fe4faf438def2d0f9f1c3770e124d3b9af1e6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-13 19:01:36 -0300

    :sparkles: feat: Adiciona componente de rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import AppRoutes from '@/routes'
 import ResetStyles from '@/styles/Reset'
 import GlobalStyles from '@/styles/Global'
 
@@ -7,5 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ResetStyles />
         <GlobalStyles />
+        <AppRoutes />
     </React.StrictMode>
 )
