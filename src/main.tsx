0076a2059e31a3e4463cf5475fbf6d66e3207da0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-13 18:59:00 -0300

    :sparkles: feat: Utiliza os estilos globais

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ResetStyles from '@/styles/Reset'
+import GlobalStyles from '@/styles/Global'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        
+        <ResetStyles />
+        <GlobalStyles />
     </React.StrictMode>
 )
