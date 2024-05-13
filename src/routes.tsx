34a6c6a1533f16766cad456b3dbfed58ffeb7792
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-13 19:00:55 -0300

    :sparkles: feat: Cria componente para as rotas da aplicação

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRoutes
