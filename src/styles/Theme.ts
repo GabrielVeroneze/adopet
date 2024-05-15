7f878273a98a4583730a5d4ab81f165dc7ecbf68
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-14 21:20:33 -0300

    :sparkles: feat: Cria tema com valores que serão utilizados no projeto

---
+++ src/styles/Theme.ts
@@ -0,0 +1,20 @@
+import { DefaultTheme } from 'styled-components'
+
+export const theme: DefaultTheme = {
+    cores: {
+        primaria: '#3772FF',
+        secundaria: '#36D6AD',
+        terciaria: '#FC7071',
+        fundo: {
+            primaria: '#FFFFFF',
+            secundaria: '#F6F6F6',
+        },
+        texto: {
+            aviso : '#FC7071',
+            claro: '#FFFFFF',
+            destaque : '#3772FF',
+            escuro : '#737380',
+            placeholder : '#BCBCBC',
+        },
+    }
+}
