204298b3f63e90edac722b0f8d60bd3316aff9ea
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-15 21:04:12 -0300

    :sparkles: feat: Utiliza o <ThemeProvider> para fornecer o tema
    Utiliza o componente <ThemeProvider> para fornecer o tema para todos os componentes da aplicação.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '@/styles/Theme'
 import AppRoutes from '@/routes'
 import ResetStyles from '@/styles/Reset'
 import GlobalStyles from '@/styles/Global'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ResetStyles />
-        <GlobalStyles />
-        <AppRoutes />
+        <ThemeProvider theme={theme}>
+            <ResetStyles />
+            <GlobalStyles />
+            <AppRoutes />
+        </ThemeProvider>
     </React.StrictMode>
 )
