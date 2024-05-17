6e3dbb572e21248ff3e53e052fb41fc68fe38e39
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-16 22:35:42 -0300

    :sparkles: feat: Cria arquivo para importar todos ícones
    Importar todos os ícones em um único arquivo para facilitar a utilização.

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,11 @@
+import iconeCasa from './casa.svg'
+import iconeMensagem from './mensagens.svg'
+import iconeUsuario from './usuario.svg'
+
+const icones = {
+    casa: iconeCasa,
+    mensagem: iconeMensagem,
+    usuario: iconeUsuario,
+}
+
+export default icones
