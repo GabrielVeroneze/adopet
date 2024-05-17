36c48cebbbccdfcc5d7b8d26f57e9ac1e8d98245
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-17 18:08:01 -0300

    :sparkles: feat: Cria componente de rodapé

---
+++ src/pages/Layout/Rodape/index.tsx
@@ -0,0 +1,17 @@
+import { Footer, Texto } from './styled'
+
+const Rodape = () => {
+    return (
+        <Footer>
+            <Texto>
+                2024 - Desenvolvido por{' '}
+                <a href="https://github.com/GabrielVeroneze" target="_blank">
+                    Gabriel Veroneze
+                </a>
+                .
+            </Texto>
+        </Footer>
+    )
+}
+
+export default Rodape
---
+++ src/pages/Layout/Rodape/styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Footer = styled.footer`
+    align-items: center;
+    background-color: ${({ theme }) => theme.cores.secundaria};
+    display: flex;
+    height: 104px;
+    justify-content: center;
+    padding: 0 ${({ theme }) => theme.espacamento.grande};
+    width: 100%;
+`
+
+export const Texto = styled.p`
+    color: ${({ theme }) => theme.cores.texto.claro};
+    font-size: ${({ theme }) => theme.fontes.tamanho.medio};
+    font-weight: ${({ theme }) => theme.fontes.peso.normal};
+    text-align: center;
+`
