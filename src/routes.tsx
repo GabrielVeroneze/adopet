17b7369fcb73819a302455c8216098c85009dc59
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-17 19:30:51 -0300

    :sparkles: feat: Adiciona Layout para as páginas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Layout from '@/pages/Layout'
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Layout />}>
+                </Route>
             </Routes>
         </Router>
     )
