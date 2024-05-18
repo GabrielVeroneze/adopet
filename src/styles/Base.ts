cfd578c7f2f7e604cce3222bb0f860c7ad9561b1
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-17 23:24:11 -0300

    :lipstick: feat: Define estilos base do botão

--- src/styles/Base.ts
+++ src/styles/Base.ts
@@ -5,3 +5,19 @@ export const BasePrincipal = styled.main`
     padding: 10.5rem 1.5rem 0;
     width: 100%;
 `
+
+export const BaseBotao = styled.button`
+    background-color: ${({ theme }) => theme.cores.terciaria};
+    border-radius: 0.375rem;
+    color: ${({ theme }) => theme.cores.texto.claro};
+    font-size: ${({ theme }) => theme.fontes.tamanho.medio};
+    font-weight: ${({ theme }) => theme.fontes.peso.semibold};
+    line-height: 1.5rem;
+    padding: 0.5rem;
+    transition-duration: 300ms;
+    transition-timing-function: ease-out;
+
+    &:hover {
+        background-color: #ff9d9e;
+    }
+`
