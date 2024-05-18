39e1b15fdb115aa35ed2469fdf2bc8261532fb65
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-17 23:42:56 -0300

    :sparkles: feat: Adiciona rota para Página Inicial

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from '@/pages/Layout'
+import Inicial from '@/pages/Inicial'
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Layout />}>
+                    <Route index element={<Inicial />} />
                 </Route>
             </Routes>
         </Router>
