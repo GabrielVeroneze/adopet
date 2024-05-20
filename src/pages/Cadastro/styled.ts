205da07c9dccea52feb120a60e06305bc96022e0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-19 21:48:32 -0300

    :lipstick: feat: Adiciona imagem de fundo

--- src/pages/Cadastro/styled.ts
+++ src/pages/Cadastro/styled.ts
@@ -1,8 +1,10 @@
 import { BasePrincipal, BaseTextoDestaque } from '@/styles/Base'
 import styled from 'styled-components'
+import imagemForma from '@/assets/images/forma-lateral.svg'
 
 export const Principal = styled(BasePrincipal)`
     align-items: center;
+    background: url(${imagemForma}) left bottom 32px no-repeat;
     background-color: ${({ theme }) => theme.cores.fundo.primaria};
     display: flex;
     flex-direction: column;
