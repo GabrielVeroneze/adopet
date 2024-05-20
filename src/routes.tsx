4d3880206de67b4c3a99dca6bef4c058296ec62e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-19 21:53:42 -0300

    :sparkles: feat: Cria rota para página de cadastro

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from '@/pages/Layout'
 import Inicial from '@/pages/Inicial'
+import Cadastro from '@/pages/Cadastro'
 
 const AppRoutes = () => {
     return (
@@ -8,6 +9,7 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Inicial />} />
+                    <Route path="cadastro" element={<Cadastro />} />
                 </Route>
             </Routes>
         </Router>
