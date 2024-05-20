20438ab5a2a8e1d0644efa923d23fdb3a78f767a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-19 21:17:00 -0300

    :lipstick: feat: Adiciona estilização do formulário

--- src/pages/Cadastro/styled.ts
+++ src/pages/Cadastro/styled.ts
@@ -23,3 +23,14 @@ export const Texto = styled.p`
     ${BaseTextoDestaque}
 `
 
+export const Formulario = styled.form`
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.espacamento.medioGrande};
+    width: 100%;
+
+    > :last-child {
+        margin-top: 0.25rem;
+    }
+`
