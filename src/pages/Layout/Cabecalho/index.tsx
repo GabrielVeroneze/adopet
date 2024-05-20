aa6f8af3538ca0df4071b9fe61c4cc5f7c1f40bb
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-19 23:42:01 -0300

    :recycle: refactor: Refatoração e renderização condicional do perfil
    Refatoração do componente Cabecalho para exibir condicionalmente o ícone de perfil de acordo com o nome da rota atual.

--- src/pages/Layout/Cabecalho/index.tsx
+++ src/pages/Layout/Cabecalho/index.tsx
@@ -1,35 +1,40 @@
+import { useLocation } from 'react-router-dom'
 import { Header, Item, Lista, Navegacao } from './styled'
 import IconeLink from '@/components/IconeLink'
 import icones from '@/assets/icons'
 
 const Cabecalho = () => {
-    const navItens = [
-        {
-            to: '#',
-            src: icones.casa,
-            alt: 'Ícone de uma casa, representando a página inicial',
-        },
-        {
-            to: '#',
-            src: icones.mensagem,
-            alt: 'Ícone de uma mensagem, representando as mensagens',
-        },
-        {
-            to: '#',
-            src: icones.usuario,
-            alt: 'Ícone de um usuário, representando a área de perfil',
-        },
-    ]
+    const { pathname } = useLocation()
+
+    const deveOcultarPerfil = ['/', '/cadastro', '/login'].includes(pathname)
 
     return (
         <Header>
             <Navegacao>
                 <Lista>
-                    {navItens.map((item, index) => (
-                        <Item key={index}>
-                            <IconeLink {...item} />
-                        </Item>
-                    ))}
+                    <Item>
+                        <IconeLink
+                            to="/home"
+                            src={icones.casa}
+                            alt="Ícone de uma casa, representando a página inicial"
+                        />
+                    </Item>
+                    <Item>
+                        <IconeLink
+                            to="/mensagem"
+                            src={icones.mensagem}
+                            alt="Ícone de uma mensagem, representando as mensagens"
+                        />
+                    </Item>
+                    <Item>
+                        {!deveOcultarPerfil && (
+                            <IconeLink
+                                to="/perfil"
+                                src={icones.usuario}
+                                alt="Ícone de um usuário, representando a área de perfil"
+                            />
+                        )}
+                    </Item>
                 </Lista>
             </Navegacao>
         </Header>
