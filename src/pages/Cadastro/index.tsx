85fd3f419c9466cbed3664b23c25202e3a57e020
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-19 21:51:49 -0300

    :sparkles: feat: Adiciona formulário de cadastro

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,4 +1,6 @@
-import { Principal, Texto, Titulo, Wrapper } from './styled'
+import { Formulario, Principal, Texto, Titulo, Wrapper } from './styled'
+import CampoDeTexto from '@/components/CampoDeTexto'
+import Botao from '@/components/Botao'
 import logo from '@/assets/images/logo-azul.svg'
 
 const Cadastro = () => {
@@ -12,6 +14,33 @@ const Cadastro = () => {
                     alguns dados:
                 </Texto>
             </Wrapper>
+            <Formulario>
+                <CampoDeTexto
+                    id="email"
+                    type="email"
+                    label="Email"
+                    placeholder="Escolha seu melhor email"
+                />
+                <CampoDeTexto
+                    id="nome"
+                    type="text"
+                    label="Nome"
+                    placeholder="Digite seu nome completo"
+                />
+                <CampoDeTexto
+                    id="senha"
+                    type="password"
+                    label="Senha"
+                    placeholder="Crie uma senha"
+                />
+                <CampoDeTexto
+                    id="confirmar-senha"
+                    type="password"
+                    label="Confirma sua senha"
+                    placeholder="Repita a senha criada acima"
+                />
+                <Botao variante="normal">Cadastrar</Botao>
+            </Formulario>
         </Principal>
     )
 }
