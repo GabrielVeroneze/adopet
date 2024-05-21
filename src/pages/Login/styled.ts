aa7fbf382ce5fcd751e0ec58be99c04913da1fd9
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-21 16:52:46 -0300

    :lipstick: feat: Cria os styled components da página Login

---
+++ src/pages/Login/styled.ts
@@ -0,0 +1,16 @@
+import { BasePrincipal, BaseTextoDestaque } from '@/styles/Base'
+import styled from 'styled-components'
+import imagemForma from '@/assets/images/forma-lateral.svg'
+
+export const Principal = styled(BasePrincipal)`
+    align-items: center;
+    background: url(${imagemForma}) left bottom 32px no-repeat;
+    background-color: ${({ theme }) => theme.cores.fundo.primaria};
+    display: flex;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.espacamento.enorme};
+`
+
+export const Titulo = styled.h1`
+    ${BaseTextoDestaque}
+`
