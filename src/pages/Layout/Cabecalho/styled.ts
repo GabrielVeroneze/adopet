a2af824859a2a12a0aa47f32120a2f8e2e4d6933
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-20 22:15:38 -0300

    :lipstick: feat: Adiciona imagem extra ao background em rotas específicas
    Cria função que verifica a rota atual e adiciona a imagem das patas ao background, apenas nas rotas de /cadastro e /login.

--- src/pages/Layout/Cabecalho/styled.ts
+++ src/pages/Layout/Cabecalho/styled.ts
@@ -1,9 +1,16 @@
 import styled from 'styled-components'
 import imagemForma from '@/assets/images/forma-cabecalho.svg'
+import imagemPatas from '@/assets/images/patas.svg'
 
-export const Header = styled.header`
-    background-image: url(${imagemForma});
-    background-repeat: no-repeat;
+const exibirImagemDeFundo = (rotaAtual: string) => {
+    if (rotaAtual === '/cadastro' || rotaAtual === '/login') {
+        return `url(${imagemPatas}) right top no-repeat,`
+    }
+}
+
+export const Header = styled.header<{ $rotaAtual: string }>`
+    background: ${({ $rotaAtual }) => exibirImagemDeFundo($rotaAtual)}
+        url(${imagemForma}) left top no-repeat;
     height: 295px;
     left: 0;
     position: fixed;
@@ -19,6 +26,7 @@ export const Lista = styled.ul`
     align-items: center;
     display: flex;
     gap: 3.625rem;
+    height: 40px;
 `
 
 export const Item = styled.li`
