3fec93d2c1b41057c4ad5e1d9c2cefc8d3d9d58a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-21 21:49:47 -0300

    :sparkles: feat: Adiciona ícones para a visibilidade do campo de senha

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,11 +1,15 @@
 import iconeCasa from './casa.svg'
 import iconeMensagem from './mensagens.svg'
 import iconeUsuario from './usuario.svg'
+import iconeOlho from './olho.svg'
+import iconeOlhoCortado from './olho-cortado.svg'
 
 const icones = {
     casa: iconeCasa,
     mensagem: iconeMensagem,
     usuario: iconeUsuario,
+    olho: iconeOlho,
+    olhoCortado: iconeOlhoCortado,
 }
 
 export default icones
