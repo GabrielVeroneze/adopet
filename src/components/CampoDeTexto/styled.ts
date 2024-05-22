66b0f0d5331f846cedc775a5022eebb92b02bd67
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-21 22:04:07 -0300

    :lipstick: feat: Cria botão de alternar visibilidade

--- src/components/CampoDeTexto/styled.ts
+++ src/components/CampoDeTexto/styled.ts
@@ -1,11 +1,13 @@
 import { BaseEntrada, BasePlaceholder } from '@/styles/Base'
 import styled from 'styled-components'
+import icones from '@/assets/icons'
 
 export const CampoWrapper = styled.div`
     align-items: center;
     display: flex;
     flex-direction: column;
     gap: ${({ theme }) => theme.espacamento.extraPequeno};
+    position: relative;
     width: 100%;
 `
 
@@ -20,4 +22,18 @@ export const Entrada = styled(BaseEntrada)`
     &::placeholder {
         ${BasePlaceholder}
     }
+`
+
+export const BotaoVisibilidade = styled.span<{ $visibilidade: boolean }>`
+    background: ${({ $visibilidade }) =>
+        $visibilidade
+            ? `url(${icones.olho}) center left 0.8px no-repeat`
+            : `url(${icones.olhoCortado}) center no-repeat`
+    };
+    bottom: 0.625rem;
+    cursor: pointer;
+    height: 18px;
+    position: absolute;
+    right: 1rem;
+    width: 18px;
 `
