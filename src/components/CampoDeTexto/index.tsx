b37e359a59f0229fd79de83968cea70a4bd4ae54
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-21 23:16:58 -0300

    :sparkles: feat: Adiciona botão e lógica para alterar a visibilidade da senha

--- src/components/CampoDeTexto/index.tsx
+++ src/components/CampoDeTexto/index.tsx
@@ -1,4 +1,5 @@
-import { CampoWrapper, Entrada, Rotulo } from './styled'
+import { useState } from 'react'
+import { BotaoVisibilidade, CampoWrapper, Entrada, Rotulo } from './styled'
 
 interface CampoDeTextoProps {
     id: string
@@ -8,10 +9,20 @@ interface CampoDeTextoProps {
 }
 
 const CampoDeTexto = ({ id, type, label, placeholder }: CampoDeTextoProps) => {
+    const [exibirSenha, setExibirSenha] = useState<boolean>(false)
+
+    const tipoInput = type === 'password' && exibirSenha ? 'text' : type
+
     return (
         <CampoWrapper>
             <Rotulo htmlFor={id}>{label}</Rotulo>
-            <Entrada id={id} type={type} placeholder={placeholder} />
+            <Entrada id={id} type={tipoInput} placeholder={placeholder} />
+            {type === 'password' && (
+                <BotaoVisibilidade
+                    $visibilidade={exibirSenha}
+                    onClick={() => setExibirSenha(!exibirSenha)}
+                />
+            )}
         </CampoWrapper>
     )
 }
