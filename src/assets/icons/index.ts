3f92fb216f6646b55f7f2c80f4337dfea93693a4
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-23 18:02:23 -0300

    :sparkles: feat: Adiciona ícone de contato

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,6 +3,7 @@ import iconeMensagem from './mensagens.svg'
 import iconeUsuario from './usuario.svg'
 import iconeOlho from './olho.svg'
 import iconeOlhoCortado from './olho-cortado.svg'
+import iconeContato from './contato.svg'
 
 const icones = {
     casa: iconeCasa,
@@ -10,6 +11,7 @@ const icones = {
     usuario: iconeUsuario,
     olho: iconeOlho,
     olhoCortado: iconeOlhoCortado,
+    contato: iconeContato,
 }
 
 export default icones
