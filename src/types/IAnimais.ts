9a50d82153f24502c3978a6227aaf61434f45727
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-22 23:22:18 -0300

    :sparkles: feat: Cria interface para tipagem de objetos animais

---
+++ src/types/IAnimais.ts
@@ -0,0 +1,10 @@
+export interface IAnimais {
+    id: number
+    imagem: string
+    nome: string
+    idade: string
+    porte: string
+    caracteristicas: string[]
+    cidade: string
+    estado: string
+}
