2799534ac2f50b8a01e9c1730dc12461ef1f743e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-23 22:03:19 -0300

    :sparkles: feat: Cria componente de ficha do animail

---
+++ src/pages/Home/FichaAnimal/index.tsx
@@ -0,0 +1,25 @@
+import { formatarCaracteristicas } from '@/utilities/formatarCaracteristicas'
+import { IAnimais } from '@/types/IAnimais'
+import { Contato, Endereco, Imagem, Info, Item, Nome } from './styled'
+
+const FichaAnimal = (props: IAnimais) => {
+    return (
+        <Item>
+            <Imagem src={props.imagem} alt={`Foto de ${props.nome}`} />
+            <Nome>{props.nome}</Nome>
+            <Info>
+                {props.idade}
+                <br />
+                Porte {props.porte}
+                <br />
+                {formatarCaracteristicas(props.caracteristicas)}
+            </Info>
+            <Endereco>
+                {props.cidade} ({props.estado})
+            </Endereco>
+            <Contato to="/mensagem">Falar com responsável</Contato>
+        </Item>
+    )
+}
+
+export default FichaAnimal
