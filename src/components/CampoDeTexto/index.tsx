8c6e8b742c3e654364d8f2df8de502b3b6f3fd49
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-25 17:52:08 -0300

    :recycle: refactor: Utiliza interface ICampoDeEntrada

--- src/components/CampoDeTexto/index.tsx
+++ src/components/CampoDeTexto/index.tsx
@@ -1,14 +1,8 @@
 import { useState } from 'react'
+import { ICampoDeEntrada } from '@/types/ICampoDeEntrada'
 import { BotaoVisibilidade, CampoWrapper, Entrada, Rotulo } from './styled'
 
-interface CampoDeTextoProps {
-    id: string
-    type: 'text' | 'email' | 'password'
-    label: string
-    placeholder: string
-}
-
-const CampoDeTexto = ({ id, type, label, placeholder }: CampoDeTextoProps) => {
+const CampoDeTexto = ({ id, type, label, placeholder }: ICampoDeEntrada) => {
     const [exibirSenha, setExibirSenha] = useState<boolean>(false)
 
     const tipoInput = type === 'password' && exibirSenha ? 'text' : type
