44b9f572376e751e3343184051fa9f5cd9e0d4a9
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-27 20:30:54 -0300

    :iphone: feat: Adapta layout para tablets

--- src/pages/Layout/Cabecalho/styled.ts
+++ src/pages/Layout/Cabecalho/styled.ts
@@ -1,15 +1,17 @@
+import { dispositivos } from '@/styles/Breakpoints'
 import styled from 'styled-components'
 import imagemForma from '@/assets/images/forma-cabecalho.svg'
 import imagemPatas from '@/assets/images/patas.svg'
 
 const exibirImagemDeFundo = (rotaAtual: string) => {
     if (rotaAtual === '/cadastro' || rotaAtual === '/login') {
-        return `url(${imagemPatas}) right top no-repeat,`
+        return `url(${imagemPatas}) right top no-repeat`
     }
+    return 'url()'
 }
 
 export const Header = styled.header<{ $rotaAtual: string }>`
-    background: ${({ $rotaAtual }) => exibirImagemDeFundo($rotaAtual)}
+    background: ${({ $rotaAtual }) => exibirImagemDeFundo($rotaAtual)},
         url(${imagemForma}) left top no-repeat;
     height: 295px;
     left: 0;
@@ -17,10 +19,20 @@ export const Header = styled.header<{ $rotaAtual: string }>`
     top: 0;
     width: 100%;
     z-index: 1;
+
+    @media ${dispositivos.tablet} {
+        background-position: right top, left -2px top;
+        background-size: 224px, 561px;
+        height: 470px;
+    }
 `
 
 export const Navegacao = styled.nav`
     padding: 2.5rem 2.25rem 0 3rem;
+
+    @media ${dispositivos.tablet} {
+        padding: 3.25rem 2.625rem 0 3rem;
+    }
 `
 
 export const Lista = styled.ul`
@@ -28,10 +40,36 @@ export const Lista = styled.ul`
     display: flex;
     gap: 3.625rem;
     height: 40px;
+
+    @media ${dispositivos.tablet} {
+        gap: 4.25rem;
+        height: 48px;
+    }
 `
 
 export const Item = styled.li`
+    &:first-child {
+        display: none;
+    }
+
     &:last-child {
         margin-left: auto;
     }
+
+    @media ${dispositivos.tablet} {
+        &:first-child {
+            display: block;
+        }
+
+        &:last-child {
+            height: 48px;
+            width: 48px;
+        }
+    }
+`
+
+export const Logo = styled.img`
+    display: block;
+    height: auto;
+    width: 128px;
 `
