18169dd8c43dc42bf018e6bf4b5301297576b7ea
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-28 17:10:33 -0300

    :iphone: feat: Adapta layout para tablets

--- src/pages/Inicial/styled.ts
+++ src/pages/Inicial/styled.ts
@@ -1,4 +1,5 @@
 import { BasePrincipal } from '@/styles/Base'
+import { dispositivos } from '@/styles/Breakpoints'
 import styled from 'styled-components'
 import imagemForma from '@/assets/images/forma-lateral-invertida.svg'
 import imagemAnimais from '@/assets/images/animais.svg'
@@ -11,9 +12,19 @@ export const Principal = styled(BasePrincipal)`
     display: flex;
     flex-direction: column;
     gap: ${({ theme }) => theme.espacamento.grande};
+
+    @media ${dispositivos.tablet} {
+        background-position: right bottom 164px, bottom -41px center;
+        background-size: 134px 664px, 338px 422px;
+        gap: ${({ theme }) => theme.espacamento.enorme};
+    }
 `
 
 export const Logo = styled.img`
+    @media ${dispositivos.tablet} {
+        height: auto;
+        width: 252px;
+    }
 `
 
 export const Wrapper = styled.div`
@@ -22,6 +33,14 @@ export const Wrapper = styled.div`
     flex-direction: column;
     gap: ${({ theme }) => theme.espacamento.medio};
     width: 248px;
+
+    @media ${dispositivos.tablet} {
+        width: 344px;
+
+        &:first-of-type {
+            gap: ${({ theme }) => theme.espacamento.grande};
+        }
+    }
 `
 
 export const Titulo = styled.h1`
@@ -30,6 +49,10 @@ export const Titulo = styled.h1`
     font-weight: ${({ theme }) => theme.fontes.peso.medium};
     line-height: 3rem;
     text-align: center;
+
+    @media ${dispositivos.tablet} {
+        font-size: ${({ theme }) => theme.fontes.tamanho.extraGrande};
+    }
 `
 
 export const Texto = styled.p`
@@ -38,4 +61,9 @@ export const Texto = styled.p`
     font-weight: ${({ theme }) => theme.fontes.peso.normal};
     line-height: 1.5rem;
     text-align: center;
+
+    @media ${dispositivos.tablet} {
+        font-size: ${({ theme }) => theme.fontes.tamanho.maior};
+        line-height: 1.625rem;
+    }
 `
