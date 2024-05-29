4e61cfc7e7b475aa747ebe9b8752c413b0841ac7
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-29 16:06:32 -0300

    :iphone: feat: Adapta layout para tablets

--- src/pages/Cadastro/styled.ts
+++ src/pages/Cadastro/styled.ts
@@ -1,6 +1,8 @@
 import { BasePrincipal, BaseTextoDestaque } from '@/styles/Base'
+import { dispositivos } from '@/styles/Breakpoints'
 import styled from 'styled-components'
 import imagemForma from '@/assets/images/forma-lateral.svg'
+import imagemFormaInvertida from '@/assets/images/forma-lateral-invertida.svg'
 
 export const Principal = styled(BasePrincipal)`
     align-items: center;
@@ -9,15 +11,30 @@ export const Principal = styled(BasePrincipal)`
     display: flex;
     flex-direction: column;
     gap: ${({ theme }) => theme.espacamento.grande};
+
+    @media ${dispositivos.tablet} {
+        background: url(${imagemFormaInvertida}) right bottom 164px / 134px
+            664px no-repeat;
+        gap: ${({ theme }) => theme.espacamento.enorme};
+    }
 `
 
 export const Logo = styled.img`
+    @media ${dispositivos.tablet} {
+        height: auto;
+        width: 252px;
+    }
 `
 
 export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     gap: ${({ theme }) => theme.espacamento.pequeno};
+
+    @media ${dispositivos.tablet} {
+        gap: 0;
+        width: 525px;
+    }
 `
 
 export const Titulo = styled.h1`
