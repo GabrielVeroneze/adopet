d1b15f14b61ec44fc0672e96d1d6bee443056ea3
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-05-29 22:55:53 -0300

    :iphone: feat: Adapta layout para tablets

--- src/pages/Contato/styled.ts
+++ src/pages/Contato/styled.ts
@@ -1,16 +1,28 @@
 import { BaseFormulario, BasePrincipal, BaseTextoDestaque } from '@/styles/Base'
+import { dispositivos } from '@/styles/Breakpoints'
 import styled from 'styled-components'
+import imagemFormaInvertida from '@/assets/images/forma-lateral-invertida.svg'
 
 export const Principal = styled(BasePrincipal)`
     align-items: center;
     display: flex;
     flex-direction: column;
     gap: ${({ theme }) => theme.espacamento.grande};
+
+    @media ${dispositivos.tablet} {
+        background: url(${imagemFormaInvertida}) right bottom 164px / 134px
+            664px no-repeat;
+        gap: ${({ theme }) => theme.espacamento.enorme};
+    }
 `
 
 export const Titulo = styled.h1`
     ${BaseTextoDestaque}
     width: 226px;
+
+    @media ${dispositivos.tablet} {
+        width: 524px;
+    }
 `
 
 export const Formulario = styled(BaseFormulario)`
