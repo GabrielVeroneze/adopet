6c46a7a079668f07c5a6dede99a0eb1948cd7d7f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-04 19:48:34 -0300

    :sparkles: feat: Utiliza o hook useFormularioCadastro e atualiza os componentes CampoDeTexto para receberem os dados de cadastro

--- src/pages/Cadastro/FormularioCadastro/index.tsx
+++ src/pages/Cadastro/FormularioCadastro/index.tsx
@@ -1,8 +1,11 @@
+import { useFormularioCadastro } from '@/hooks/useFormularioCadastro'
 import { Formulario } from './styled'
 import CampoDeTexto from '@/components/CampoDeTexto'
 import Botao from '@/components/Botao'
 
 const FormularioCadastro = () => {
+    const { cadastroDados, handleDadosChange } = useFormularioCadastro()
+
     return (
         <Formulario>
             <CampoDeTexto
@@ -10,24 +13,32 @@ const FormularioCadastro = () => {
                 type="email"
                 label="Email"
                 placeholder="Escolha seu melhor email"
+                value={cadastroDados.email}
+                onChange={handleDadosChange}
             />
             <CampoDeTexto
                 name="nome"
                 type="text"
                 label="Nome"
                 placeholder="Digite seu nome completo"
+                value={cadastroDados.nome}
+                onChange={handleDadosChange}
             />
             <CampoDeTexto
                 name="senha"
                 type="password"
                 label="Senha"
                 placeholder="Crie uma senha"
+                value={cadastroDados.senha}
+                onChange={handleDadosChange}
             />
             <CampoDeTexto
                 name="confirmarSenha"
                 type="password"
                 label="Confirma sua senha"
                 placeholder="Repita a senha criada acima"
+                value={cadastroDados.confirmarSenha}
+                onChange={handleDadosChange}
             />
             <Botao variante="normal">Cadastrar</Botao>
         </Formulario>
