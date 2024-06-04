d94b7c3605dcf6c346ae61a91f82d1afde82a746
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-04 19:26:34 -0300

    :sparkles: feat: Adicionada manipulação de estado e eventos

--- src/components/CampoDeTexto/index.tsx
+++ src/components/CampoDeTexto/index.tsx
@@ -2,7 +2,14 @@ import { useState } from 'react'
 import { ICampoDeEntrada } from '@/types/ICampoDeEntrada'
 import { BotaoVisibilidade, CampoWrapper, Entrada, Rotulo } from './styled'
 
-const CampoDeTexto = ({ name, type, label, placeholder }: ICampoDeEntrada) => {
+const CampoDeTexto = ({
+    name,
+    type,
+    label,
+    placeholder,
+    value,
+    onChange,
+}: ICampoDeEntrada) => {
     const [exibirSenha, setExibirSenha] = useState<boolean>(false)
 
     const tipoInput = type === 'password' && exibirSenha ? 'text' : type
@@ -10,7 +17,14 @@ const CampoDeTexto = ({ name, type, label, placeholder }: ICampoDeEntrada) => {
     return (
         <CampoWrapper>
             <Rotulo htmlFor={name}>{label}</Rotulo>
-            <Entrada id={name} name={name} type={tipoInput} placeholder={placeholder} />
+            <Entrada
+                id={name}
+                name={name}
+                type={tipoInput}
+                placeholder={placeholder}
+                value={value}
+                onChange={evento => onChange(evento.target)}
+            />
             {type === 'password' && (
                 <BotaoVisibilidade
                     $visibilidade={exibirSenha}
