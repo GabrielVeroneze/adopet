41777ff77cca753d6dc940bb794c226d63793a78
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-06 17:55:57 -0300

    :sparkles: feat: adiciona validações ao schema de cadastro

--- src/hooks/useCadastroForm.ts
+++ src/hooks/useCadastroForm.ts
@@ -2,12 +2,34 @@ import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
-const cadastroSchema = z.object({
-    email: z.string().min(3),
-    nome: z.string().min(3),
-    senha: z.string().min(3),
-    confirmarSenha: z.string().min(3),
-})
+const cadastroSchema = z
+    .object({
+        email: z
+            .string()
+            .min(1, { message: 'O campo email é obrigatório.' })
+            .email({ message: 'Insira um endereço de email válido.' }),
+        nome: z
+            .string()
+            .min(1, { message: 'O campo nome é obrigatório.' })
+            .min(3, { message: 'O nome deve ter pelo menos 3 caracteres.' })
+            .max(50, { message: 'O nome não pode ter mais de 50 caracteres.' })
+            .regex(/^[a-zA-Z ]+$/, {
+                message: 'O nome só pode conter letras e espaços.',
+            }),
+        senha: z
+            .string()
+            .min(1, { message: 'O campo senha é obrigatório.' })
+            .regex(/^(?=.*[A-Z])(?=.*[0-9]).{8,}$/, {
+                message: 'A senha deve ter pelo menos 8 caracteres, incluir uma letra maiúscula e um número.',
+            }),
+        confirmarSenha: z
+            .string()
+            .min(1, { message: 'O campo confirmar senha é obrigatório.' }),
+    })
+    .refine(({ senha, confirmarSenha }) => senha === confirmarSenha, {
+        message: 'As senhas precisam ser iguais.',
+        path: ['confirmarSenha'],
+    })
 
 type CadastroSchemaType = z.infer<typeof cadastroSchema>
 
