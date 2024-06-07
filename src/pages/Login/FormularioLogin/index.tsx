39ccac430670a698ce9c7177d5b547c39999ed09
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-06 22:19:38 -0300

    :sparkles: feat: Utiliza hook useLoginForm para gerenciar o formulário

--- src/pages/Login/FormularioLogin/index.tsx
+++ src/pages/Login/FormularioLogin/index.tsx
@@ -1,21 +1,26 @@
+import { useLoginForm } from '@/hooks/useLoginForm'
 import { Formulario, Link } from './styled'
 import CampoDeTexto from '@/components/CampoDeTexto'
 import Botao from '@/components/Botao'
 
 const FormularioLogin = () => {
+    const { register, handleSubmit, errors, fazerLogin } = useLoginForm()
+
     return (
-        <Formulario>
+        <Formulario onSubmit={handleSubmit(fazerLogin)}>
             <CampoDeTexto
-                name="email"
+                id="email"
                 type="email"
                 label="Email"
                 placeholder="Insira seu email"
+                register={register('email')}
             />
             <CampoDeTexto
-                name="senha"
+                id="senha"
                 type="password"
                 label="Senha"
                 placeholder="Insira sua senha"
+                register={register('senha')}
             />
             <Link to="#">Esqueci minha senha.</Link>
             <Botao variante="normal">Entrar</Botao>
