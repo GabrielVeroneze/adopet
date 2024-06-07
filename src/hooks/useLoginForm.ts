4fe74e5cff6cd5a52cc86bebd42f9e59c25935f8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-06 22:27:54 -0300

    :sparkles: feat: adiciona validação com Zod ao hook

--- src/hooks/useLoginForm.ts
+++ src/hooks/useLoginForm.ts
@@ -1,15 +1,25 @@
+import { z } from 'zod'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const loginSchema = z.object({
+    email: z.string(),
+    senha: z.string(),
+})
+
+type LoginSchemaType = z.infer<typeof loginSchema>
 
 export const useLoginForm = () => {
     const {
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm({
+    } = useForm<LoginSchemaType>({
         mode: 'onTouched',
+        resolver: zodResolver(loginSchema),
     })
 
-    const fazerLogin = (dados) => {
+    const fazerLogin = (dados: LoginSchemaType) => {
         console.log(dados)
     }
 
