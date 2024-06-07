363d39c4b99bab4d2010fefd1de6614d0cb7a136
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-06 22:36:48 -0300

    :sparkles: feat: adiciona validações ao schema de login

--- src/hooks/useLoginForm.ts
+++ src/hooks/useLoginForm.ts
@@ -3,8 +3,16 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 const loginSchema = z.object({
-    email: z.string(),
-    senha: z.string(),
+    email: z
+        .string()
+        .min(1, { message: 'O campo email é obrigatório.' })
+        .email({ message: 'Insira um endereço de email válido.' }),
+    senha: z
+        .string()
+        .min(1, { message: 'O campo senha é obrigatório.' })
+        .regex(/^(?=.*[A-Z])(?=.*[0-9]).{8,}$/, {
+            message: 'A senha deve ter pelo menos 8 caracteres, incluir uma letra maiúscula e um número.',
+        }),
 })
 
 type LoginSchemaType = z.infer<typeof loginSchema>
