009d57fb838d14b4a432e9fc79807a740d62d143
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-08 19:50:47 -0300

    :sparkles: feat: adiciona ToastContainer para exibição de notificações

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '@/styles/Theme'
 import AppRoutes from '@/routes'
 import ResetStyles from '@/styles/Reset'
 import GlobalStyles from '@/styles/Global'
+import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <ResetStyles />
             <GlobalStyles />
+            <ToastContainer />
             <AppRoutes />
         </ThemeProvider>
     </React.StrictMode>
