590e91887a9ebc8a0a8dffc7c0430dba140ec7a6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-09 23:27:03 -0300

    :recycle: refactor: altera lógica de cadastro
    Remove lógica de navegação e notificações, para que useAutenticacao permaneça apenas com a lógica de autenticação

--- src/hooks/useAutenticacao.ts
+++ src/hooks/useAutenticacao.ts
@@ -1,24 +1,18 @@
-import { useNavigate } from 'react-router-dom'
 import { jsonServerApi } from '@/services/api'
-import { notificarUsuario } from '@/utilities/mostrarNotificacao'
 import { IUsuario } from '@/types/IUsuario'
 
 export const useAutenticacao = () => {
-    const navigate = useNavigate()
 
     const fazerCadastro = async ({ nome, email, senha }: IUsuario) => {
-        try {
-            const resposta = await jsonServerApi.post('/usuarios', {
-                nome,
-                email,
-                senha,
-            })
+        const resposta = await jsonServerApi.post<IUsuario>('/usuarios', {
+            nome,
+            email,
+            senha,
+        })
 
-            if (resposta.status === 201) {
-                navigate('/home')
-            }
-        } catch (error) {
-            notificarUsuario('error', 'Erro ao cadastrar usuário')
+        return resposta.status === 201
+    }
+
         }
     }
 
