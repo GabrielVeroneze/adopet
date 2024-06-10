94b64d107e80083c330b9cc89b61fcfcb8e10a54
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-09 23:29:12 -0300

    :sparkles: feat: adiciona função fazerLogin para autenticação de usuário

--- src/hooks/useAutenticacao.ts
+++ src/hooks/useAutenticacao.ts
@@ -1,7 +1,10 @@
 import { jsonServerApi } from '@/services/api'
+import { useUsuario } from '@/context/usuario/useUsuario'
+import { setTokenLocalStorage } from '@/utilities/tokenLocalStorage'
 import { IUsuario } from '@/types/IUsuario'
 
 export const useAutenticacao = () => {
+    const { setUsuario } = useUsuario()
 
     const fazerCadastro = async ({ nome, email, senha }: IUsuario) => {
         const resposta = await jsonServerApi.post<IUsuario>('/usuarios', {
@@ -13,10 +16,25 @@ export const useAutenticacao = () => {
         return resposta.status === 201
     }
 
+    const fazerLogin = async ({ email, senha }: IUsuario) => {
+        const resposta = await jsonServerApi.get<IUsuario[]>(
+            `/usuarios?email=${email}&senha=${senha}`
+        )
+
+        const usuario = resposta.data[0]
+
+        if (usuario) {
+            setTokenLocalStorage(usuario.id!)
+            setUsuario(usuario)
+
+            return true
         }
+
+        return false
     }
 
     return {
         fazerCadastro,
+        fazerLogin,
     }
 }
