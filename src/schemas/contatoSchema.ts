412bdfca8d6f62c6cfe5d9e054baac5ad85f49b5
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-11 18:18:34 -0300

    :sparkles: feat: adiciona validações detalhadas ao esquema contatoSchema

--- src/schemas/contatoSchema.ts
+++ src/schemas/contatoSchema.ts
@@ -1,10 +1,27 @@
 import { z } from 'zod'
 
 export const contatoSchema = z.object({
-    nome: z.string(),
-    telefone: z.string(),
-    nomeAnimal: z.string(),
-    mensagem: z.string(),
+    nome: z
+        .string()
+        .min(1, { message: 'O campo nome é obrigatório.' })
+        .min(3, { message: 'O nome deve ter pelo menos 3 caracteres.' })
+        .max(50, { message: 'O nome não pode ter mais de 50 caracteres.' }),
+    telefone: z
+        .string()
+        .min(1, { message: 'O campo telefone é obrigatório.' })
+        .regex(/\(?\d{2}\)?\s?\d{4,5}[\s-]?\d{4}/, {
+            message: 'O telefone não é válido.',
+        }),
+    nomeAnimal: z
+        .string()
+        .min(1, { message: 'O campo nome do animal é obrigatório.' })
+        .min(3, { message: 'O nome do animal deve ter pelo menos 3 caracteres.' })
+        .max(50, { message: 'O nome do animal não pode ter mais de 50 caracteres.' }),
+    mensagem: z
+        .string()
+        .min(1, { message: 'O campo mensagem é obrigatório.' })
+        .min(10, { message: 'A mensagem deve ter pelo menos 10 caracteres.' })
+        .max(500, { message: 'A mensagem não pode ter mais de 500 caracteres.' }),
 })
 
 export type ContatoSchemaType = z.infer<typeof contatoSchema>
