c10cd1be77b528acb5626c13581978e3e3237bde
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-10 23:25:26 -0300

    :sparkles: feat: adiciona lógica de validação de token no UsuarioProvider

--- src/context/usuario/UsuarioProvider.tsx
+++ src/context/usuario/UsuarioProvider.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useAutenticacao } from '@/hooks/useAutenticacao'
+import { getTokenLocalStorage } from '@/utilities/tokenLocalStorage'
 import { IUsuario } from '@/types/IUsuario'
 import { UsuarioContext } from './UsuarioContext'
 
@@ -8,6 +10,19 @@ interface UsuarioProviderProps {
 
 export const UsuarioProvider = ({ children }: UsuarioProviderProps) => {
     const [usuario, setUsuario] = useState<IUsuario | null>(null)
+    const { validarToken } = useAutenticacao()
+
+    useEffect(() => {
+        (async () => {
+            const token = getTokenLocalStorage()
+            if (token) {
+                const usuario = await validarToken(token)
+                if (usuario) {
+                    setUsuario(usuario)
+                }
+            }
+        })()
+    }, [validarToken])
 
     return (
         <UsuarioContext.Provider value={{ usuario, setUsuario }}>
