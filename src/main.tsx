3bc072859fc634a2cc3dfde13245c4cd640b838c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-10 23:13:39 -0300

    :sparkles: feat: utiliza UsuarioProvider para fornecer contexto de usuário em toda a aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ToastContainer } from 'react-toastify'
 import { ThemeProvider } from 'styled-components'
+import { UsuarioProvider } from '@/context/usuario/UsuarioProvider'
 import { theme } from '@/styles/Theme'
 import AppRoutes from '@/routes'
 import ResetStyles from '@/styles/Reset'
@@ -14,7 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <ResetStyles />
             <GlobalStyles />
             <ToastContainer />
-            <AppRoutes />
+            <UsuarioProvider>
+                <AppRoutes />
+            </UsuarioProvider>
         </ThemeProvider>
     </React.StrictMode>
 )
