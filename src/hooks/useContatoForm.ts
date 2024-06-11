0c955d91d8012683276d486f48d148423bb5835f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-11 16:54:15 -0300

    :sparkles: feat: integra o schema de contato ao gerenciador do formulário

--- src/hooks/useContatoForm.ts
+++ src/hooks/useContatoForm.ts
@@ -1,9 +1,14 @@
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ContatoSchemaType, contatoSchema } from '@/schemas/contatoSchema'
 
 export const useContatoForm = () => {
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit } = useForm<ContatoSchemaType>({
+        mode: 'onTouched',
+        resolver: zodResolver(contatoSchema),
+    })
 
-    const enviar = (dados) => {
+    const enviar = (dados: ContatoSchemaType) => {
         console.log(dados)
     }
 
