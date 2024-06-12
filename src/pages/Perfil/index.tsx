363264236db370a274e0622d85ae67375f6daa76
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-11 23:08:29 -0300

    :sparkles: feat: integra hook usePerfilForm ao formulário de perfil

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,3 +1,4 @@
+import { usePerfilForm } from '@/hooks/usePerfilForm'
 import { Formulario, Principal, Subtitulo, Titulo } from './styled'
 import CampoUploadFoto from '@/pages/Perfil/CampoUploadFoto'
 import CampoTextoFormulario from '@/components/CampoTextoFormulario'
@@ -5,37 +6,43 @@ import AreaTextoFormulario from '@/components/AreaTextoFormulario'
 import Botao from '@/components/Botao'
 
 const Perfil = () => {
+    const { register, handleSubmit, salvar } = usePerfilForm()
+
     return (
         <Principal>
             <Titulo>
                 Esse é o perfil que aparece para responsáveis ou ONGs que
                 recebem sua mensagem.
             </Titulo>
-            <Formulario>
+            <Formulario onSubmit={handleSubmit(salvar)}>
                 <Subtitulo>Perfil</Subtitulo>
-                <CampoUploadFoto />
+                <CampoUploadFoto register={register('foto')} />
                 <CampoTextoFormulario
                     id="nome"
                     type="text"
                     label="Nome"
                     placeholder="Digite seu nome completo"
+                    register={register('nome')}
                 />
                 <CampoTextoFormulario
                     id="telefone"
                     type="tel"
                     label="Telefone"
                     placeholder="Digite seu telefone"
+                    register={register('telefone')}
                 />
                 <CampoTextoFormulario
                     id="cidade"
                     type="text"
                     label="Cidade"
                     placeholder="Digite o nome da sua cidade"
+                    register={register('cidade')}
                 />
                 <AreaTextoFormulario
                     id="sobre"
                     label="Sobre"
                     placeholder="Escreva sobre você"
+                    register={register('sobre')}
                 />
                 <Botao variante="normal">Salvar</Botao>
             </Formulario>
