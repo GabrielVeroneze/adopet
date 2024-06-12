5520c9be82e9e4e507279570dfcdaa60c57d7fe2
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-12 16:30:51 -0300

    :sparkles: feat: integra perfilSchema ao hook usePerfilForm

--- src/hooks/usePerfilForm.ts
+++ src/hooks/usePerfilForm.ts
@@ -1,9 +1,14 @@
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { PerfilSchemaType, perfilSchema } from '@/schemas/perfilSchema'
 
 export const usePerfilForm = () => {
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit } = useForm<PerfilSchemaType>({
+        mode: 'onTouched',
+        resolver: zodResolver(perfilSchema),
+    })
 
-    const salvar = (dados) => {
+    const salvar = (dados: PerfilSchemaType) => {
         console.log(dados)
     }
 
