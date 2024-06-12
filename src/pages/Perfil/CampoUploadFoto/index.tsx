72429cf4cd457950cbffa7865fe465217a878eac
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-11 23:05:43 -0300

    :recycle: refactor: adiciona suporte à integração com react-hook-form no componente

--- src/pages/Perfil/CampoUploadFoto/index.tsx
+++ src/pages/Perfil/CampoUploadFoto/index.tsx
@@ -1,13 +1,18 @@
+import { UseFormRegisterReturn } from 'react-hook-form'
 import { FotoUsuario, InputUpload, Texto, Wrapper } from './styled'
 import LabelFormulario from '@/components/LabelFormulario'
 import icones from '@/assets/icons'
 
-const CampoUploadFoto = () => {
+interface CampoUploadFotoProps {
+    register: UseFormRegisterReturn
+}
+
+const CampoUploadFoto = ({ register }: CampoUploadFotoProps) => {
     return (
         <Wrapper>
             <LabelFormulario>Foto</LabelFormulario>
             <FotoUsuario $imagem={icones.usuario}>
-                <InputUpload accept="image/*" />
+                <InputUpload accept="image/*" {...register} />
             </FotoUsuario>
             <Texto>Clique na foto para editar</Texto>
         </Wrapper>
