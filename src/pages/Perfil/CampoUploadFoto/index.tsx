ce70c0c7a6f057bef69c51e94b437e6a94306f3a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-13 17:48:27 -0300

    :recycle: refactor: modifica CampoUploadFoto para usar transformarImagemEmUrl
    Modifica componente para receber a propriedade `imagem` em vez de utilizar o ícone de usuário padrão diretamente.

--- src/pages/Perfil/CampoUploadFoto/index.tsx
+++ src/pages/Perfil/CampoUploadFoto/index.tsx
@@ -1,17 +1,18 @@
 import { UseFormRegisterReturn } from 'react-hook-form'
+import { transformarImagemEmUrl } from '@/utilities/transformarImagemEmUrl'
 import { FotoUsuario, InputUpload, Texto, Wrapper } from './styled'
 import LabelFormulario from '@/components/LabelFormulario'
-import icones from '@/assets/icons'
 
 interface CampoUploadFotoProps {
+    imagem: FileList
     register: UseFormRegisterReturn
 }
 
-const CampoUploadFoto = ({ register }: CampoUploadFotoProps) => {
+const CampoUploadFoto = ({ imagem, register }: CampoUploadFotoProps) => {
     return (
         <Wrapper>
             <LabelFormulario>Foto</LabelFormulario>
-            <FotoUsuario $imagem={icones.usuario}>
+            <FotoUsuario $imagem={transformarImagemEmUrl(imagem)}>
                 <InputUpload accept="image/*" {...register} />
             </FotoUsuario>
             <Texto>Clique na foto para editar</Texto>
