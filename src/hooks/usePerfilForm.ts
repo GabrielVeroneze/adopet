3727c8017d62c328b290686edaf395121085afe5
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-15 15:44:28 -0300

    :sparkles: feat: Adiciona lógica de atualização de perfil do usuário

--- src/hooks/usePerfilForm.ts
+++ src/hooks/usePerfilForm.ts
@@ -1,8 +1,15 @@
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { PerfilSchemaType, perfilSchema } from '@/schemas/perfilSchema'
+import { useUsuario } from '@/context/usuario/useUsuario'
+import { jsonServerApi } from '@/services/api'
+import { transformarImagemEmUrl } from '@/utilities/transformarImagemEmUrl'
+import { notificarUsuario } from '@/utilities/mostrarNotificacao'
+import { IUsuario } from '@/types/IUsuario'
 
 export const usePerfilForm = () => {
+    const { usuario, setUsuario } = useUsuario()
+
     const {
         register,
         handleSubmit,
@@ -14,7 +21,22 @@ export const usePerfilForm = () => {
     })
 
     const salvar = (dados: PerfilSchemaType) => {
-        console.log(dados)
+        const perfilFormatado = {
+            ...dados,
+            foto: transformarImagemEmUrl(dados.foto),
+        }
+
+        jsonServerApi
+            .patch<IUsuario>(`/usuarios/${usuario!.id}`, {
+                perfil: perfilFormatado,
+            })
+            .then(resposta => {
+                setUsuario(resposta.data)
+                notificarUsuario('success', 'Perfil atualizado com sucesso')
+            })
+            .catch(() => {
+                notificarUsuario('error', 'Erro ao atualizar perfil')
+            })
     }
 
     return {
