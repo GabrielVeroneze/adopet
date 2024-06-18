768d9db54cb574292c107481bbf937a9542e2abc
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-06-18 19:00:14 -0300

    :lipstick: feat: ajusta estilos de CustomLink e Imagem

--- src/components/IconeLink/styled.ts
+++ src/components/IconeLink/styled.ts
@@ -4,10 +4,13 @@ import styled from 'styled-components'
 export const CustomLink = styled(Link)`
     align-items: center;
     display: flex;
+    height: 100%;
     justify-content: center;
+    width: 100%;
 `
 
 export const Imagem = styled.img`
-    height: auto;
+    height: 100%;
+    object-fit: cover;
     width: 100%;
 `
