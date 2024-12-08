c7b6dbf80428a9ccb7ec028449aed0838e553729
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-12-07 21:19:37 -0300

    ♻️ refactor: refatora hook `usePerfilForm` para utilizar serviço de atualização de perfil

--- src/hooks/usePerfilForm.ts
+++ src/hooks/usePerfilForm.ts
@@ -3,10 +3,9 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { PerfilSchemaType, perfilSchema } from '@/schemas/perfilSchema'
 import { useUsuario } from '@/context/usuario/useUsuario'
+import { atualizarPerfil } from '@/services/usuario'
 import { converterBase64EmFile, converterImagemEmBase64 } from '@/utilities/converterImagem'
 import { notificarUsuario } from '@/utilities/mostrarNotificacao'
-import { Usuario } from '@/types/Usuario'
-import api from '@/services/api'
 
 export const usePerfilForm = () => {
     const { usuario, setUsuario } = useUsuario()
@@ -32,22 +31,21 @@ export const usePerfilForm = () => {
     }, [usuario, reset])
 
     const salvar = async (dados: PerfilSchemaType) => {
-        const fotoFormatada = await converterImagemEmBase64(dados.foto)
+        const novosDadosPerfil = {
+            ...dados,
+            foto: await converterImagemEmBase64(dados.foto),
+        }
 
-        api
-            .patch<Usuario>(`/usuarios/${usuario!.id}`, {
-                perfil: {
-                    ...dados,
-                    foto: fotoFormatada,
-                },
-            })
-            .then(resposta => {
-                setUsuario(resposta.data)
-                notificarUsuario('success', 'Perfil atualizado com sucesso')
-            })
-            .catch(() => {
-                notificarUsuario('error', 'Erro ao atualizar perfil')
-            })
+        try {
+            const usuarioAtualizado = await atualizarPerfil(usuario.id, novosDadosPerfil)
+
+            setUsuario(usuarioAtualizado)
+            notificarUsuario('success', 'Perfil atualizado com sucesso')
+        } catch (erro) {
+            if (erro instanceof Error) {
+                notificarUsuario('error', erro.message)
+            }
+        }
     }
 
     return {
